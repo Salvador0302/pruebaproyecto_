06bf5fbf25bde85711e6b3447e3648fe222deeed
Baku-D-Shaggy <diego.mouchet@gmail.com>
2025-06-10 18:57:58 +0000

    prediccion mejorada

--- app2.py
+++ app2.py
@@ -143,11 +143,11 @@ if st.button(texts['predict_button'], type="primary"):
         prob_percent = prob * 100
 
         # Determinando categoria segun los rangos
-        if prob_percent <= 55:
+        if prob_percent <= 42:
             message = texts['no_diabetes']
             color_style = "success"
             recommendations = texts['no_diabetes_rec']
-        elif 55 < prob_percent < 85:
+        elif 42 < prob_percent < 69:
             message = texts['prediabetes']
             color_style = "warning"
             recommendations = texts['prediabetes_rec']
